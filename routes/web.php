a336f7b7e3ba81d9dde814575802574ee6054470
pinof <fattoruso.g@gmail.com>
2021-02-21 20:31:14 +0100

    add new category

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\CategoryComponent;
 use App\Http\Livewire\SearchComponent;
 use App\Http\Livewire\Admin\AdminCategoryComponent;
+use App\Http\Livewire\Admin\AdminAddCategoryComponent;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -57,5 +58,6 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function(){
 Route::middleware(['auth:sanctum', 'verified' ])->group(function(){
     Route::get('/admin/dashboard' , AdminDashboardComponent::class)->name('admin.dashboard');
     Route::get('/admin/categories' , AdminCategoryComponent::class)->name('admin.categories');
+    Route::get('/admin/category/add' , AdminAddCategoryComponent::class)->name('admin.addcategory');
 });
 
