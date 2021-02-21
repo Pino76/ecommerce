74b80a13b5f3a658af3d5329f24e3b408a6dfb2a
pinof <fattoruso.g@gmail.com>
2021-02-21 19:19:44 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Livewire\HomeComponent;
+use App\Http\Livewire\ShopComponent;
+use App\Http\Livewire\CartComponent;
+use App\Http\Livewire\CheckoutComponent;
+use App\Http\Livewire\DetailsComponent;
+use App\Http\Livewire\User\UserDashboardComponent;
+use App\Http\Livewire\Admin\AdminDashboardComponent;
+use App\Http\Livewire\CategoryComponent;
+use App\Http\Livewire\SearchComponent;
+use App\Http\Livewire\Admin\AdminCategoryComponent;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/*Route::get('/', function () {
+    return view('welcome');
+});*/
+
+Route::get('/' , HomeComponent::class);
+
+Route::get('/shop' , ShopComponent::class);
+
+Route::get('/cart' , CartComponent::class)->name('product.cart');
+
+Route::get('/checkout' , CheckoutComponent::class);
+
+Route::get('/product/{slug}', DetailsComponent::class)->name('product.details');
+
+Route::get('/product-category/{category_slug}' , CategoryComponent::class)->name('product.category');
+
+Route::get('/search', SearchComponent::class)->name('product.search');
+
+/*
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+*/
+
+//For User or Customer
+Route::middleware(['auth:sanctum', 'verified'])->group(function(){
+    Route::get('/user/dashboard', UserDashboardComponent::class)->name('user.dashboard');
+});
+
+
+//For Admin
+#Route::middleware(['auth:sanctum', 'verified' , 'authadmin'])->group(function(){
+Route::middleware(['auth:sanctum', 'verified' ])->group(function(){
+    Route::get('/admin/dashboard' , AdminDashboardComponent::class)->name('admin.dashboard');
+    Route::get('/admin/categories' , AdminCategoryComponent::class)->name('admin.categories');
+});
+
