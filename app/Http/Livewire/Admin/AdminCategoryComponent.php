bcae7bfa872b195372b0ee3862577a22465a8c19
pinof <fattoruso.g@gmail.com>
2021-03-06 18:41:23 +0100

    delete category admin

--- app/Http/Livewire/Admin/AdminCategoryComponent.php
+++ app/Http/Livewire/Admin/AdminCategoryComponent.php
@@ -9,10 +9,15 @@ use Livewire\WithPagination;
 class AdminCategoryComponent extends Component{
 
     use WithPagination;
-    public function render(){
 
+    public function deleteCategory($id){
+        $category = Category::find($id);
+        $category->delete();
+        session()->flash('message','Category has been deleted successfully!!!');
+    }
+
+    public function render(){
         $categories = Category::paginate(5);
         return view('livewire.admin.admin-category-component', ['categories'=>$categories])->layout('layouts.base');
-
     }
 }
