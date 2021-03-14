432895b70ce1efd69441e41ae2ea1133b2b016ee
pinof <fattoruso.g@gmail.com>
2021-03-14 18:03:48 +0100

    admin delete Product

--- app/Http/Livewire/Admin/AdminProductComponent.php
+++ app/Http/Livewire/Admin/AdminProductComponent.php
@@ -9,6 +9,13 @@ use Livewire\WithPagination;
 class AdminProductComponent extends Component {
 
     use WithPagination;
+
+    public function deleteProduct($id){
+        $product = Product::find($id);
+        $product->delete();
+        session()->flash('message','Product has been deleted successfull!!!');
+    }
+
     public function render(){
         $products = Product::paginate(10);
         return view('livewire.admin.admin-product-component', ["products"=>$products])->layout('layouts.base');
