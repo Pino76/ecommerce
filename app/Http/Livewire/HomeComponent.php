18d0095dc6b4a839030b31ac7eb192ce6f32dd8a
pinof <fattoruso.g@gmail.com>
2021-03-18 21:29:15 +0100

    admin show latest product on homepage

--- app/Http/Livewire/HomeComponent.php
+++ app/Http/Livewire/HomeComponent.php
@@ -3,13 +3,15 @@
 namespace App\Http\Livewire;
 
 use App\Models\HomeSlider;
+use App\Models\Product;
 use Livewire\Component;
 
 class HomeComponent extends Component{
 
     public function render(){
         $sliders = HomeSlider::where('status' , 1)->get();
-        return view('livewire.home-component',["sliders"=>$sliders])->layout('layouts.base');
+        $lproducts = Product::orderBy('created_at' , 'DESC')->get()->take(8);
+        return view('livewire.home-component',["sliders" => $sliders ,  "lproducts" => $lproducts])->layout('layouts.base');
     }
 
 }
